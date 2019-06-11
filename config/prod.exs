57cf336694de981a8b937b10ec2998357757e761
Kodeklubben <sfjkk@fiontar.net>
2019-06-11 19:48:26 +0200

    Update prod config (Heroku)

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 #Database configuration
 config: platform, Platform.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"), pool_size: String.to_integer(System.get_env("POOL_SIZE ||10")), ssl:true
+  url: System.get_env("DATABASE_URL"), pool_size: String.to_integer(System.get_env("POOL_SIZE ||10")), ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
