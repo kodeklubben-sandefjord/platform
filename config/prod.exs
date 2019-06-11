bf4ffaeabc1b63c43bf5590f98363ea4191ea5b7
Kodeklubben <sfjkk@fiontar.net>
2019-06-11 19:50:34 +0200

    Update prod config (Heroku)

--- config/prod.exs
+++ config/prod.exs
@@ -16,8 +16,8 @@ force_ssl: [rewrite_on: [:x_forwarded_proto]],
 cache_static_manifest: "priv/static/cache_manifest.json",
 secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-#Database configuration
-config: platform, Platform.Repo,
+# Database configuration
+config :platform, Platform.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"), pool_size: String.to_integer(System.get_env("POOL_SIZE ||10")), ssl: true
 
