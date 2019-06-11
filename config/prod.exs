0b08a0d619ac3151405206c701f314552f81e61b
Kodeklubben <sfjkk@fiontar.net>
2019-06-11 19:52:22 +0200

    Update prod config (Heroku)

--- config/prod.exs
+++ config/prod.exs
@@ -10,11 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :platform, PlatformWeb.Endpoint,
-http: [inet6, port: System.get_env("PORT") || 4000],
-url: [scheme: "https", host: "kodeklubben.herokuapp.com", port: 443],
-force_ssl: [rewrite_on: [:x_forwarded_proto]],
-cache_static_manifest: "priv/static/cache_manifest.json",
-secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [scheme: "https", host: "kodeklubben.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Database configuration
 config :platform, Platform.Repo,
